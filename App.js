6e10e8c823cd7adf16ddccf8734345e1a484c4e8
FernandoTimo <fertimo99@gmail.com>
2021-07-27 17:03:46 -0500

    app

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import style from './src/styles/app.styles';
-import Icon from './src/public/svg/score.svg';
+import Icon from './src/public/svg/icon.svg';
 import {Screen} from './src/components/timoideas/Timoideas.component';
 
 export default function App() {
   return (
     <Screen>
       <View style={style.Container}>
-        <Icon width={150} height={150} fill={'#111'} />
+        <Icon width={30} height={30} fill={'#111'} />
         <Text style={style.Title}>App</Text>
       </View>
     </Screen>
