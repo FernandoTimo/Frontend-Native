f3b5b0d706ede03a15293a19c07406325fd1a2dd
FernandoTimo <fertimo99@gmail.com>
2021-07-27 14:47:30 -0500

    Prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
+  endOfLine: 'auto',
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
-	endOfLine: 'auto',
 };
